03034a551c1d15f93b43cf2093904341cb1ca46c
hana demma <hana.demma@gmail.com>
2024-12-19 22:17:51 +0300

    outlier removal

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -52,3 +52,68 @@ def replace_missing_values(data):
             data_copy[column].fillna(column_mode, inplace=True)
 
     return data_copy
+
+
+def get_outlier_summary(data):
+    """
+    Calculates outlier summary statistics for a DataFrame.
+
+    Args:
+        data : Input DataFrame.
+
+    Returns:
+        Outlier summary DataFrame.
+    """
+
+    outlier_summary = pd.DataFrame(columns=['Variable', 'Number of Outliers'])
+    data = data.select_dtypes(include='number')
+
+    for column_name in data.columns:
+        q1 = data[column_name].quantile(0.25)
+        q3 = data[column_name].quantile(0.75)
+        iqr = q3 - q1
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+        outliers = data[(data[column_name] < lower_bound) | (data[column_name] > upper_bound)]
+
+        outlier_summary = pd.concat(
+            [outlier_summary, pd.DataFrame({'Variable': [column_name], 'Number of Outliers': [outliers.shape[0]]})],
+            ignore_index=True
+        )
+    non_zero_count = (outlier_summary['Number of Outliers'] > 0).sum()
+    print(f"From {data.shape[1]} selected numerical columns, there are {non_zero_count} columns with outlier values.")
+
+    return outlier_summary
+
+def getBoxPlotToCheckOutlier(data, variables):
+    for variable in variables:
+        if pd.api.types.is_numeric_dtype(data[variable]):
+            sns.boxplot(data=data, x=variable, orient='v')
+            plt.title(f'Box Plot of {variable}')
+            plt.xlabel('Values')
+            plt.show()
+        else:
+            print(f"Skipping non-numeric column: {variable}")
+
+
+def remove_outliers_winsorization(xdr_data):
+    """
+    Removes outliers from specified columns of a DataFrame using winsorization.
+
+    Args:
+        data: The input DataFrame.
+        column_names (list): A list of column names to process.
+
+    Returns:
+        The DataFrame with outliers removed.
+    """
+    # data = xdr_data.select_dtypes(include='number')
+    for column_name in xdr_data.select_dtypes(include='number').columns:
+        q1 = xdr_data[column_name].quantile(0.25)
+        q3 = xdr_data[column_name].quantile(0.75)
+        iqr = q3 - q1
+        lower_bound = q1 - 1.5 * iqr
+        upper_bound = q3 + 1.5 * iqr
+        xdr_data[column_name] = xdr_data[column_name].clip(lower_bound, upper_bound)
+
+    return xdr_data
